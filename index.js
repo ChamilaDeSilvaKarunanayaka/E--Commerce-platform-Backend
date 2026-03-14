5097009ada7f38b93a76de7c8172bf66e62f3c5e
DCDS Karunanayaka <kdchamiladesilva@gmail.com>
2026-03-15 00:27:56 +0530

    chore: update server to use nodemon for automatic restarts and add CRUD endpoints

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
 import express from 'express';
 
-let app = express();
-app.listen(3000, () => {
+const app = express();
+
+app.get("/",
+    (req, res)=>{
+        res.json(
+            {
+                message : "this is a get request"
+            }
+        )
+    }
+)
+app.delete("/",
+    (req, res)=>{
+        res.json(
+            {
+                message : "this is a delete request"
+            }
+        )
+    }
+)
+app.post("/",
+    (req, res)=>{
+        res.json(
+            {
+                message : "this is a post request"
+            }
+        )
+    }
+)
+app.put("/",
+    (req, res)=>{
+        res.json(   
+            {
+                message : "this is a put request"
+            }
+        )
+    }
+)
+
+
+app.listen(3000, ()=>{      // Start the server and listen on port 3000
     console.log('Server is running on port 3000');
-});
+}  ) 
+
